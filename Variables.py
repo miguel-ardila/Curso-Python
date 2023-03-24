b5c1e1eb8a903d9ad10cb5314dc92b7673cc76b7
Miguel Ardila <miguel_sxe@hotmail.com>
2023-03-23 19:00:35 -0500

    variables

--- Variables.py
+++ Variables.py
@@ -10,3 +10,13 @@ z = x + y
 print(x)
 print(y)
 print(z)
+
+# uso de Variables
+student = "Juan Perez"
+phonenumber = "55621817"
+email = "jperez@mail.com"
+#para imprimir las varias separadas con , se agrega el string(str) antes de cada variable al hacer el print.
+#La clase str en Python se utiliza para representar texto, más conocido en el mundo de la programación como string o cadena de caracteres.
+
+print(str(student) + "," + str(phonenumber) + "," + str(email))
+

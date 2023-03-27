6db678465b8066771843b4d9722d8c08eff7d702
Miguel Ardila <miguel_sxe@hotmail.com>
2023-03-27 14:28:47 -0500

    manejo de cadenas

---
+++ menejo-de-cadenas.py
@@ -0,0 +1,9 @@
+#cadena_(string)
+migrupofavorito = "Los Beatles"
+print("Mi grupo favorito es: "+ migrupofavorito)
+
+#otro ejemplo
+myfavoriteband = "Blink 182"
+commet = "The Best Punk Rock Band"
+print("My favorite band is: "+ myfavoriteband + " " + commet)
+

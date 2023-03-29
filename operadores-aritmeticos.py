192b28bfa40d004f2e73ed174a8be1fe00c05c3c
Miguel Ardila <miguel_sxe@hotmail.com>
2023-03-29 16:33:10 -0500

    aritmetico

---
+++ operadores-aritmeticos.py
@@ -0,0 +1,16 @@
+#operadores aritmeticos
+# + suma
+# - resta
+# * multiplicacion
+# / division
+# % modulo
+# ** potencia
+# // division entera
+
+#ejemplo de suma
+
+operandoA = 5
+operandoB = 3
+suma = operandoA + operandoB
+print("El resultado de la suma es: ", suma)
+print(f"El resultado de la suma es: {suma}")

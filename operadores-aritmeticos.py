57f956e265797e2f8af668dfea35212ecb3662d0
Miguel Ardila <miguel_sxe@hotmail.com>
2023-03-29 16:57:07 -0500

    aritmeticos

--- operadores-aritmeticos.py
+++ operadores-aritmeticos.py
@@ -9,10 +9,37 @@
 
 #ejemplo de suma
 
-operandoA = 5
-operandoB = 3
+operandoA = 3
+operandoB = 2
 suma = operandoA + operandoB
 print("El resultado de la suma es: ", suma)
 print(f"El resultado de la suma es: {suma}") #{suma} se le llama interpolacion (incluir una variable dentro de un string) no olvidar el f antes del string para que funcione la interpolacion.
 
+
+
+#ejemplo de resta
+resta = operandoA - operandoB
+print(f"resultado de la resta es: {resta} ")
+
+#ejemplo de multiplicacion
+multiplicacion = operandoA * operandoB
+print(f"resultado de la multiplicacion es: {multiplicacion} ")
+
+#ejemplo de division
+division = operandoA / operandoB
+division = operandoA // operandoB #division entera
+print(f"resultado de la division es: {division}")
+print(f"resultado de la division es (int): {division} ")
+
+#ejemplo de modulo
+modulo = operandoA % operandoB
+print(f"resultado del modulo es: {modulo} ")
+
+#ejemplo de potencia
+potencia = operandoA ** operandoB
+print(f"resultado de la potencia es: {potencia} ")
+
+#ejemplo de division entera
+divisionEntera = operandoA // operandoB
+print(f"resultado de la division entera es: {divisionEntera} ")
 

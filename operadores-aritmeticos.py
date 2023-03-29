25c8f3561b40e7f544945e8b9f5484dc22641c5b
Miguel Ardila <miguel_sxe@hotmail.com>
2023-03-29 16:41:21 -0500

    aritmeticos

--- operadores-aritmeticos.py
+++ operadores-aritmeticos.py
@@ -13,4 +13,6 @@ operandoA = 5
 operandoB = 3
 suma = operandoA + operandoB
 print("El resultado de la suma es: ", suma)
-print(f"El resultado de la suma es: {suma}")
+print(f"El resultado de la suma es: {suma}") #{suma} se le llama interpolacion (incluir una variable dentro de un string) no olvidar el f antes del string para que funcione la interpolacion.
+
+

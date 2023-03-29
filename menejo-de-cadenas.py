f20c7b3eb5fcbd291748f8513bdba0f89bfa3aaa
Miguel Ardila <miguel_sxe@hotmail.com>
2023-03-29 11:28:29 -0500

    manejo-cadenas

--- menejo-de-cadenas.py
+++ menejo-de-cadenas.py
@@ -7,3 +7,17 @@ myfavoriteband = "Blink 182"
 commet = "The Best Punk Rock Band"
 print("My favorite band is: "+ myfavoriteband + " " + commet)
 
+
+
+numero1 = "10"
+numero2 = "20"
+print(numero1 + numero2)
+
+numero1 = 1
+numero2 = 2
+print(numero1 + numero2)
+
+number = "20"
+number2 = "30"
+print("Concatenacion:", number + number2)
+print("Suma:", int(number) + int(number2))
